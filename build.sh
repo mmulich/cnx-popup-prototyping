0407a2e090a856d73cd499a4ba1de2bf7cb0eee6
Michael Mulich <michael.mulich@gmail.com>
2012-08-27 09:27:44 -0400

    Build script to pull in Aloha and custom plugins.

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Use this Build script to put everything in the right place.
+
+git clone https://github.com/wysiwhat/Aloha-Editor.git ../Aloha-Editor
+git clone https://github.com/oerpub/oerpub.editor.git ../oerpub.editor
