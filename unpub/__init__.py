9eb8f6499bfc5dcd4456e22d8f4f9a1f794c1099
Michael Mulich <michael.mulich@gmail.com>
2012-08-29 15:23:47 -0400

    Default home view

--- unpub/__init__.py
+++ unpub/__init__.py
@@ -4,10 +4,14 @@ from pyramid.config import Configurator
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
+
     """
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.include('pyramid_jinja2')
     config.add_jinja2_search_path('unpub:templates/')
+    # Views / Routes
+    config.add_route('casa', '/')
+
     config.scan()
     return config.make_wsgi_app()
