04f098b43b820eca7df9a41aa84655b032b843ee
Michael Mulich <michael.mulich@gmail.com>
2012-08-30 09:27:02 -0400

    The editor page with the aloha-editor resources reference

--- unpub/__init__.py
+++ unpub/__init__.py
@@ -8,10 +8,18 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
+    # XXX Temporary resource locations until we determine how to best
+    #     distribute the aloha-editor code base.
+    config.add_static_view('Aloha-Editor',
+                           settings['unpub.aloha_editor_resource'])
+
+    # Include the Jinja2 integration package
     config.include('pyramid_jinja2')
     config.add_jinja2_search_path('unpub:templates/')
+
     # Views / Routes
     config.add_route('casa', '/')
+    config.add_route('edit', '/edit')
 
     config.scan()
     return config.make_wsgi_app()
--- unpub/views.py
+++ unpub/views.py
@@ -2,10 +2,15 @@
 from pyramid.view import view_config
 
 
-SITE_TITLE = 'Connexions Web Viewer'
+SITE_TITLE = 'Connexions Authoring Services'
 
 
 @view_config(route_name='casa', renderer='casa.jinja2')
 def casa(request):
     """The home page for this application."""
+    return {'title': SITE_TITLE}
+
+@view_config(route_name='edit', renderer='edit.jinja2')
+def edit(request):
+    """The editor page for this application."""
     return {'title': SITE_TITLE}
